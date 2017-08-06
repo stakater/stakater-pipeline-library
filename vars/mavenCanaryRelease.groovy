535c064eb96e65d1621654e08a7eaa0101b5e644
Rasheed Amir <rasheed@aurorasolutions.io>
2017-08-06 22:51:33 +0200

    try dev profile instead of prod

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -26,7 +26,7 @@ def call(body) {
     sh "./mvnw org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${config.version}"
 
     // what is happening here?
-    sh "./mvnw clean -e -U deploy -Pprod -DskipTests"
+    sh "./mvnw clean -e -U deploy -Pdev -DskipTests"
 
     // TODO: what is this doing?
     junitResults(body)
