25c4ec7b21abf521337b6f325ccaa7c83078713e
Waseem <waseemhassan@outlook.com>
2017-11-28 19:37:44 +0500

    Remove empty spaces

--- vars/mavenIntegrationTestWithExistingNamespace.groovy
+++ vars/mavenIntegrationTestWithExistingNamespace.groovy
@@ -13,13 +13,10 @@ def call(body) {
     kubeNS = "-Dkubernetes.namespace=${ns} -Dfabric8.use.existing=${ns}"
     echo "Running the integration tests in the namespace: ${ns}"
 
-
     if (utils.isDisabledITests()) {
         echo "WARNING: Integration tests are current DISABLED for these pipelines!"
-
     } else {
         sh "./mvnw org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:integration-test ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:verify"
-
         junitResults(body);
     }
   }
