693dc4994d13dcd31673c404e9ca660014fdb6b2
Waseem Hassan <waseemhassan@outlook.com>
2017-12-06 12:03:49 +0500

    Remove disabled IT tests check

--- vars/mavenIntegrationTestWithExistingNamespace.groovy
+++ vars/mavenIntegrationTestWithExistingNamespace.groovy
@@ -14,15 +14,11 @@ def call(body) {
     kubeNS = "-Dkubernetes.namespace=${ns} -Dfabric8.use.existing=${ns}"
     echo "Running the integration tests in the namespace: ${ns}"
 
-    if (utils.isDisabledITests()) {
-        echo "WARNING: Integration tests are current DISABLED for these pipelines!"
-    } else {
-        if(args) {
-            sh "./mvnw org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:integration-test ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} ${args} org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:verify"
-        }
-        else {
-            sh "./mvnw org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:integration-test ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:verify"
-        }
-        junitResults(body);
+    if(args) {
+      sh "./mvnw org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:integration-test ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} ${args} org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:verify"
     }
-  }
+    else {
+      sh "./mvnw org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:integration-test ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} org.apache.maven.plugins:maven-failsafe-plugin:2.18.1:verify"
+    }
+    junitResults(body);
+}
