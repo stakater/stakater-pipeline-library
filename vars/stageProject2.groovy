b9177d59e73c0d9668df4e698510fde722c0e20b
asim <asimawan.42@gmail.com>
2017-12-11 16:12:41 +0500

    add stageProject2

---
+++ vars/stageProject2.groovy
@@ -0,0 +1,51 @@
+#!/usr/bin/groovy
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def flow = new io.stakater.StakaterCommands()
+  def repoId
+  def releaseVersion
+  def extraStageImages = config.extraImagesToStage ?: []
+  def extraSetVersionArgs = config.setVersionExtraArgs ?: ""
+  def containerName = config.containerName ?: 'maven'
+
+  container(name: containerName) {
+
+    sh 'chmod 600 /root/.ssh-git/ssh-key'
+    sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
+    sh 'chmod 700 /root/.ssh-git'
+    sh 'chmod 600 /home/jenkins/.gnupg/pubring.gpg'
+    sh 'chmod 600 /home/jenkins/.gnupg/secring.gpg'
+    sh 'chmod 600 /home/jenkins/.gnupg/trustdb.gpg'
+    sh 'chmod 700 /home/jenkins/.gnupg'
+
+    sh "git remote set-url origin git@github.com:${config.project}.git"
+
+    def currentVersion = flow.getProjectVersion()
+
+    flow.setupWorkspaceForRelease(config.project, config.useGitTagForNextVersion, extraSetVersionArgs, currentVersion)
+
+    repoId = flow.stageSonartypeRepo()
+    releaseVersion = flow.getProjectVersion()
+
+    // lets avoide the stash / unstash for now as we're not using helm ATM
+    //stash excludes: '*/src/', includes: '**', name: "staged-${config.project}-${releaseVersion}".hashCode().toString()
+
+    if (!config.useGitTagForNextVersion){
+      flow.updateGithub ()
+    }
+  }
+
+  if (config.extraImagesToStage != null){
+    stageExtraImages {
+      images = extraStageImages
+      tag = releaseVersion
+    }
+  }
+
+  return [config.project, releaseVersion, repoId]
+}
