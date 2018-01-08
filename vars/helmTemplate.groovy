af953e2aaabbfb8d8995c5bbd9972902aea8665a
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-01-08 17:56:32 +0500

    Add helm node

---
+++ vars/helmTemplate.groovy
@@ -0,0 +1,71 @@
+#!/usr/bin/groovy
+import io.fabric8.Fabric8Commands
+import io.fabric8.Utils
+
+def call(Map parameters = [:], body) {
+    def flow = new Fabric8Commands()
+    def utils = new Utils()
+
+    def defaultLabel = buildId('helm')
+    def label = parameters.get('label', defaultLabel)
+
+    def helmImage = parameters.get('helmImage', 'stakater/helm-bundle:1.0')
+    def jnlpImage = 'jenkinsci/jnlp-slave:2.62'
+    def inheritFrom = parameters.get('inheritFrom', 'base')
+
+    def cloud = flow.getCloudConfig()
+
+    // 0.13 introduces a breaking change when defining pod env vars so check version before creating build pod
+    if (utils.isKubernetesPluginVersion013()) {
+        echo "Kubernetes Plugin Version 013"
+        echo "building using the docker socket"
+
+        podTemplate(cloud: cloud,
+                label: label,
+                inheritFrom: "${inheritFrom}",
+                containers: [
+                        containerTemplate (
+                                name: 'helm',
+                                image: "${helmImage}",
+                                command: '/bin/sh -c',
+                                args: 'cat',
+                                ttyEnabled: true,
+                                alwaysPullImage: false,
+                                workingDir: '/home/helm/',
+                                envVars: [
+                                        envVar(key: 'DOCKER_CONFIG', value: '/home/helm/.docker/')])],
+                volumes: [
+                        secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/helm/.docker'),
+                        secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/helm/.gnupg'),
+                        secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/helm/.apitoken'),
+                        secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                        secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+                        hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')])
+            {
+              body()
+            }
+    } else {
+
+        echo "building using the docker socket"
+
+        podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}",
+                containers: [
+                        //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
+                        [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
+                          envVars: [
+                                  [key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn']]]],
+                volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
+                          persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
+                          secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                          secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
+                          secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                          secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
+                          secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
+                          hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
+                envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
+            ) {
+                body()
+            }
+    }
+
+}
