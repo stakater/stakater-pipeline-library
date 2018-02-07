0403a3b33a8b258d5c9f030664ed6b86a94c7023
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-02-07 12:44:22 +0500

    Change to class

--- src/io/stakater/Common.groovy
+++ src/io/stakater/Common.groovy
@@ -1,10 +1,14 @@
 #!/usr/bin/groovy
 package io.stakater
 
-static def shOutput(String command) {
-    return sh(
-        script: """
-            ${command}
-        """,
-        returnStdout: true).toString().trim()
+class Common {
+    
+    static def shOutput(String command) {
+        return sh(
+            script: """
+                ${command}
+            """,
+            returnStdout: true).toString().trim()
+    }
+
 }
