b7caa44fec81b7400a40d8cee4a0711aecb04fb3
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-02-08 11:33:24 +0500

    Update common

--- src/io/stakater/Common.groovy
+++ src/io/stakater/Common.groovy
@@ -1,14 +1,12 @@
 #!/usr/bin/groovy
-package io.stakater
+package io.stakater  
 
-class Common {
-    
-    static def shOutput(String command) {
-        return sh(
-            script: """
-                ${command}
-            """,
-            returnStdout: true).toString().trim()
-    }
-
+static def shOutput(String command) {
+    return sh(
+        script: """
+            ${command}
+        """,
+        returnStdout: true).toString().trim()
 }
+
+return this
