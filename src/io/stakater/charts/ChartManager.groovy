ae75e95969606a5648978d483d7bf152451b982c
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-02-09 15:31:05 +0500

    Update ChartManager

--- src/io/stakater/charts/ChartManager.groovy
+++ src/io/stakater/charts/ChartManager.groovy
@@ -1,12 +1,7 @@
 #!/usr/bin/groovy
 package io.stakater.charts
 
-
-def pushToChartMuseum(String location, String chartName) {
-    def helm = new io.stakater.charts.Helm()
-    helm.lint(location, chartName)
-    String fileName = helm.package(location, chartName)
-
+def uploadToChartMuseum(String location, String chartName, String fileName) {
     def chartMuseum = new io.stakater.charts.ChartMuseum()
     chartMuseum.upload(location, chartName, fileName)
 }
