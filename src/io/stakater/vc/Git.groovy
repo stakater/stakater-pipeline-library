d8d6633d7ec4bccb16ae739169bd1b62ffd91705
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-02-14 12:43:33 +0500

    Move git methods

---
+++ src/io/stakater/vc/Git.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/groovy
+package io.stakater.vc
+
+def setUserInfo(String gitUserName, String gitUserEmail) {
+    sh """
+        git config --global user.name "${gitUserName}"
+        git config --global user.email "${gitUserEmail}"
+    """
+}
+
+def addHostsToKnownHosts() {
+    sh """
+        mkdir -p /root/.ssh/
+        echo -e "Host github.com\\n\\tStrictHostKeyChecking no\\n" > /root/.ssh/config
+        echo -e "Host gitlab.com\\n\\tStrictHostKeyChecking no\\n" >> /root/.ssh/config
+        ssh-keyscan github.com > /root/.ssh/known_hosts
+        echo "\n" >> /root/.ssh/known_hosts
+        ssh-keyscan gitlab.com >> /root/.ssh/known_hosts
+    """
+}
+
+def commitChanges(String repoDir, String commitMessage) {
+    String messageToCheck = "nothing to commit, working tree clean"
+    sh """
+        chmod 600 /root/.ssh-git/ssh-key
+        eval `ssh-agent -s`
+        ssh-add /root/.ssh-git/ssh-key
+        cd ${repoDir}
+        git add .
+        if ! git status | grep '${messageToCheck}' ; then
+            git commit -m "${commitMessage}"
+            git push
+        else
+            echo \"nothing to do\"
+        fi
+    """
+}
+
+def checkoutRepo(String repoUrl, String branch, String dir) {
+    sh """
+        chmod 600 /root/.ssh-git/ssh-key
+        eval `ssh-agent -s`
+        ssh-add /root/.ssh-git/ssh-key
+        
+        rm -rf ${dir}
+        git clone -b ${branch} ${repoUrl} ${dir}
+    """
+}
+
+return this
