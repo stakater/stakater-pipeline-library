fad2dc05da41c3096f88debc21dadb42ce25977f
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-12 17:09:08 +0500

    Add auth upload functions for chartmuseum

--- src/io/stakater/charts/ChartManager.groovy
+++ src/io/stakater/charts/ChartManager.groovy
@@ -6,4 +6,9 @@ def uploadToChartMuseum(String location, String chartName, String fileName) {
     chartMuseum.upload(location, chartName, fileName)
 }
 
+def uploadToChartMuseum(String location, String chartName, String fileName, String cmUsername, String cmPassword) {
+    def chartMuseum = new io.stakater.charts.ChartMuseum()
+    chartMuseum.upload(location, chartName, fileName, cmUsername, cmPassword)
+}
+
 return this
