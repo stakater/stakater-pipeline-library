c1c7c820be683be3d7402fe162b83e98885c3fa5
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-30 12:00:02 +0500

    Update ws dir

--- vars/withCurrentRepo.groovy
+++ vars/withCurrentRepo.groovy
@@ -7,15 +7,19 @@ def call(Map parameters = [:], body) {
         def gitUsername = parameters.get('gitUsername', 'stakater-user')
         def gitEmail = parameters.get('gitEmail', 'stakater@gmail.com')
 
-        def workspaceDir = pwd() + repoName
+        def workspaceDir = "/home/jenkins/" + repoName
 
-        ws(workspaceDir) {
-            def git = new io.stakater.vc.Git()
-            
-            git.setUserInfo(gitUsername, gitEmail)
-            git.addHostsToKnownHosts()
-            git.checkoutRepo(repoUrl, repoBranch, workspaceDir)
-            body()
+        sh "mkdir -p ${workspaceDir}"
+
+        dir(workspaceDir) {
+            ws(workspaceDir) {
+                def git = new io.stakater.vc.Git()
+                
+                git.setUserInfo(gitUsername, gitEmail)
+                git.addHostsToKnownHosts()
+                git.checkoutRepo(repoUrl, repoBranch, workspaceDir)
+                body()
+            }
         }
     }
 }
