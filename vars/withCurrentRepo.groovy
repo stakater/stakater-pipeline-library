7c60700da03980a2f3d5de2d377628b043a60548
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-30 11:52:29 +0500

    Add checkout inside withCurrentRepo

--- vars/withCurrentRepo.groovy
+++ vars/withCurrentRepo.groovy
@@ -1,9 +1,21 @@
-def call(body) {
+def call(Map parameters = [:], body) {
     withSCM { def repoUrl, def repoName, def repoBranch ->
         print "Repo URL ${repoUrl}"
         print "Repo Name ${repoName}"
         print "Repo Branch ${repoBranch}"
 
-        body()
+        def gitUsername = parameters.get('gitUsername', 'stakater-user')
+        def gitEmail = parameters.get('gitEmail', 'stakater@gmail.com')
+
+        def workspaceDir = pwd() + repoName
+
+        ws(workspaceDir) {
+            def git = new io.stakater.vc.Git()
+            
+            git.setUserInfo(gitUsername, gitEmail)
+            git.addHostsToKnownHosts()
+            git.checkoutRepo(repoUrl, repoBranch, workspaceDir)
+            body()
+        }
     }
 }
