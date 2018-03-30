d4fbc66bfdb25964363cdc9be1c4d3fd26a7af4a
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-30 12:32:29 +0500

    Add ssh url conversion

--- vars/withSCM.groovy
+++ vars/withSCM.groovy
@@ -3,6 +3,17 @@ def call(body) {
     
     def repoUrl = scmConfig.getUrl()
     def repoName = repoUrl.tokenize('/').last().tokenize('.').first()    
+
+    if(!repoUrl.startsWith("git@")) {
+        // Lets make it ssh url link
+        //https://github.com/Stakater/IngressMonitorController.git
+        def url = new java.net.URL(repoUrl)
+        print "Host: ${url.getHost()}"
+        print " Path: ${url.getPath()}"
+
+        repoUrl = "git@${url.getHost()}:${url.getPath()}" 
+    }
+
     def repoBranch = scmConfig.getRefspec().tokenize('/').last()
 
     body(repoUrl, repoName, repoBranch)
