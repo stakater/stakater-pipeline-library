6c37e581c326c73f8448755fbb99582bf4c4e4fc
hazim1093 <hazim_malik@hotmail.com>
2018-05-03 16:53:24 +0500

    Add methods to render templates

---
+++ src/io/stakater/charts/Templates.groovy
@@ -0,0 +1,27 @@
+
+def renderChart(String chartTemplatesDir, String chartPath){
+    sh """
+        for template in ${chartTemplatesDir}/*.yaml.tmpl; do 
+            # Remove .tmpl suffix
+            file=\${template%.tmpl}
+            # Remove full path
+            file=\${file##*/}
+            # Append new path
+            file="${chartPath}/\${file}"
+            # Render template
+            gotplenv \${template} > \${file}
+        done
+    """
+}
+
+def generateManifests(String chartPath, String manifestsDir){
+    sh """
+        mkdir -p ${manifestsDir}
+        templatesDir="templates"
+        for templateName in ${chartPath}/\${templatesDir}/*.yaml; do
+            # Remove full path
+            templateName=\${templateName##*/}
+            helm template ${chartPath} -x \${templatesDir}/\${templateName} > ${manifestsDir}/\${templateName}
+        done
+    """
+}
