d8326f83b7fd8580b6da898f848c07d25f1aa054
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-05-11 16:13:39 +0500

    Fix tools node creating extra slaves

--- vars/toolsWithCurrentKubeNode.groovy
+++ vars/toolsWithCurrentKubeNode.groovy
@@ -4,9 +4,12 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('tools')
     def label = parameters.get('label', defaultLabel)
 
-    toolsWithCurrentKubeTemplate(parameters) {
-        node(label) {
-            body()
+    podTemplate(name: 'sa-secret',
+        volumes: [secretVolume(secretName: "k8s-current-cluster-kubeconfig", mountPath: '/home/jenkins/.kube')]) {
+        toolsTemplate(parameters) {
+            node(label) {
+                body()
+            }
         }
     }
 }
