30ca7ec6bc6e77ba5576a48f27a7ee07173459da
faizanahmad055 <faizan.ahmad55@outlook.com>
2018-06-11 16:24:58 +0500

    Update symlinkDir global variable value

--- vars/withCurrentRepo.groovy
+++ vars/withCurrentRepo.groovy
@@ -7,9 +7,8 @@ def call(Map parameters = [:], body) {
 
         def gitUsername = parameters.get('gitUsername', 'stakater-user')
         def gitEmail = parameters.get('gitEmail', 'stakater@gmail.com')
-
         def workspaceDir = "/home/jenkins/" + repoName
-        def symlinkDir = "/go/src/${host}/${repoOwner}"
+        def symlinkDir = ""
         sh "mkdir -p ${workspaceDir}"
 
         def type = parameters.get('type', '')
@@ -18,6 +17,7 @@ def call(Map parameters = [:], body) {
             // Symlink workspaceDir in gopath if go project
             case "go":
                 def host = repoUrl.substring(repoUrl.indexOf("@") + 1, repoUrl.indexOf(":"))
+                symlinkDir = "/go/src/${host}/${repoOwner}"
                 def symlink = "${symlinkDir}/${repoName}"
                 println("Creating symlink of ${workspaceDir} at ${symlink} for go build")
                 sh """
