bfb9707c4554bd5d8c72df1f21b59e9b88aa2c39
kahootali <kahoot.ali@gmail.com>
2018-10-04 15:51:49 +0500

    fix owner

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -24,7 +24,7 @@ def call(body) {
         container(name: 'tools') {
             withCurrentRepo { def repoUrl, def repoName, def repoOwner, def repoBranch ->
                 def imageName = repoName.split("dockerfile-").last().toLowerCase()
-                if (repoOwner.startsWith('stakater-docker'){
+                if (repoOwner.startsWith('stakater-docker')){
                     repoOwner = 'stakater'
                 }
                 
