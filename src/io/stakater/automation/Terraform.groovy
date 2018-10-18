7fb4d10cec3fc4185f87c817e97690344cdf67ea
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-10-18 16:25:55 +0500

    Move variable inside method

--- src/io/stakater/automation/Terraform.groovy
+++ src/io/stakater/automation/Terraform.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/groovy
 package io.stakater.automation
 
-def defaultProviders = [
-  "https://github.com/stakater/terraform-provider-gitlab/releases/download/v1.1.0/terraform-provider-gitlab"
-]
 
 def installDefaultThirdPartyProviders() {
+  def defaultProviders = [
+    "https://github.com/stakater/terraform-provider-gitlab/releases/download/v1.1.0/terraform-provider-gitlab"
+  ]
   installThirdPartyProviders(defaultProviders)
 }
 
