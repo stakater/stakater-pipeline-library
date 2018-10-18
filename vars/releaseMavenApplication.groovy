48f4098a7c05d6988dbaaa9644d0f7e2796a110c
kahootali <kahoot.ali@gmail.com>
2018-10-18 18:01:48 +0500

    update version

--- vars/releaseMavenApplication.groovy
+++ vars/releaseMavenApplication.groovy
@@ -6,7 +6,7 @@ def call(body) {
     body.delegate = config
     body()
 
-    toolsNode(toolsImage: 'stakater/builder-maven:3.5.4-jdk1.8-apline8-v0.0.2') {
+    toolsNode(toolsImage: 'stakater/builder-maven:3.5.4-jdk1.8-apline8-v0.0.3') {
         
         def builder = new io.stakater.builder.Build()
         def docker = new io.stakater.containers.Docker()
