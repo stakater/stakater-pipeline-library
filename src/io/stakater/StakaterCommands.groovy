043bf43c9a7622db061c2c41501d37efdf40947d
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-10-22 19:51:43 +0500

    fixes slash encoding

--- src/io/stakater/StakaterCommands.groovy
+++ src/io/stakater/StakaterCommands.groovy
@@ -157,9 +157,9 @@ def extractOrganizationAndProjectFromGitHubUrl(url) {
     } else if (url.contains("git@gitlab.com:")){
         url = url.replaceAll("git@gitlab.com:", '')
     }
-    
+
     if (url.contains("gitlab.com")){
-        url = url.replaceAll("/", "%2F")
+        url = url.replaceAll("\\/", "%2F")
     }
 
     if (url.contains(".git")){
