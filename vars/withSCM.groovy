6f385e036198951ba49fff3d7e04ecd61c2f3680
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-10-24 16:35:01 +0500

    adds test echo statements

--- vars/withSCM.groovy
+++ vars/withSCM.groovy
@@ -1,5 +1,8 @@
 def call(body) {
     def utils = new io.fabric8.Utils()
+    def git = new io.stakater.vc.Git()
+    def flow = new io.stakater.StakaterCommands()
+
     def scmConfig = scm.getUserRemoteConfigs()[0]
     
     def repoUrl = scmConfig.getUrl()
@@ -8,6 +11,18 @@ def call(body) {
     def repoOwner = tokenizedUrl.get(tokenizedUrl.size() - 2)
 
     echo "INSIDE SCM"
+    echo "Branch Name: ${env.BRANCH_NAME}"
+
+    def url = flow.getScmPushUrl()
+    echo "URL: ${url}"
+    def provider = flow.getProvider(url)
+    echo "provider: ${provider}"
+
+    def project = flow.getProject(provider)
+    echo "project name with organization: ${project}"
+
+    def providerToken = flow.getProviderToken(provider)
+    echo "provider-token: ${providerToken}"
 
     if(!repoUrl.startsWith("git@")) {
         // Lets make it ssh url link
