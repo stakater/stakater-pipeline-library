0d12da2961b88f14586bad45deeaed4260438a82
kahootali <kahoot.ali@gmail.com>
2018-11-02 15:43:46 +0500

    add params

--- vars/e2eTestStage.groovy
+++ vars/e2eTestStage.groovy
@@ -5,7 +5,9 @@ def call(Map parameters = [:], config) {
         def jobName = parameters.get('jobName', 'carbook/e2e-tests-manual/master')
         def chartName = parameters.get('chartName', '')
         def chartVersion = parameters.get('chartVersion', '')
-        def testJob = build job: jobName, parameters: [ [$class: 'StringParameterValue', name: 'chartName', value: chartName ], [$class: 'StringParameterValue', name: 'chartVersion', value: chartVersion ], [$class: 'StringParameterValue', name: 'config', value: JsonOutput.toJson(config) ]], propagate:false
+        def repoUrl = parameters.get('repoUrl', '')
+        def repoBranch = parameters.get('repoBranch', '')
+        def testJob = build job: jobName, parameters: [ [$class: 'StringParameterValue', name: 'chartName', value: chartName ], [$class: 'StringParameterValue', name: 'chartVersion', value: chartVersion ], [$class: 'StringParameterValue', name: 'repoUrl', value: repoUrl ], [$class: 'StringParameterValue', name: 'repoBranch', value: repoBranch ], [$class: 'StringParameterValue', name: 'config', value: JsonOutput.toJson(config) ]], propagate:false
 
         node {
             String text = "<h2>Regression test</h2><a href=\"${testJob.getAbsoluteUrl()}\">${testJob.getProjectName()} ${testJob.getDisplayName()} - ${testJob.getResult()}</a>"
