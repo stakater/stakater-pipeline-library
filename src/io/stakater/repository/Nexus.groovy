b44c95217df9ffb18866282ad305f796e266cf8a
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-11-10 10:14:18 +0500

    Fix directories in nexus

--- src/io/stakater/repository/Nexus.groovy
+++ src/io/stakater/repository/Nexus.groovy
@@ -4,7 +4,7 @@ package io.stakater.repository
 def pushAppArtifact(String appName, String version) {
   def repositoryType = version.contains("SNAPSHOT") ? "snapshots" : "releases"
   sh """
-    mvn deploy:deploy-file -DgeneratePom=false -DrepositoryId=nexus -Durl=http://nexus.tools/repository/maven-${repositoryType} -DpomFile=application/pom.xml -Dfile=application/target/${appName}-${version}.jar
+    mvn deploy:deploy-file -DgeneratePom=false -DrepositoryId=nexus -Durl=http://nexus.tools/repository/maven-${repositoryType} -DpomFile=pom.xml -Dfile=target/${appName}-${version}.jar
   """
 }
 
