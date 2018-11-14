b7b2d012834f31a3521033cb9b8ff8faba9280e3
kahootali <kahoot.ali@gmail.com>
2018-11-14 15:40:21 +0500

    remove node

--- vars/e2eTestStage.groovy
+++ vars/e2eTestStage.groovy
@@ -11,10 +11,8 @@ def call(Map parameters = [:], config) {
     def repoBranch = parameters.get('repoBranch', '')
     def testJob = build job: e2eJobName, parameters: [ [$class: 'StringParameterValue', name: 'chartName', value: chartName ], [$class: 'StringParameterValue', name: 'chartVersion', value: chartVersion ], [$class: 'StringParameterValue', name: 'repoUrl', value: repoUrl ], [$class: 'StringParameterValue', name: 'repoBranch', value: repoBranch ],  [$class: 'StringParameterValue', name: 'performanceTestJobName', value: performanceTestJobName ],  [$class: 'StringParameterValue', name: 'appName', value: appName ],  [$class: 'StringParameterValue', name: 'mockAppsJobName', value: mockAppsJobName ], [$class: 'StringParameterValue', name: 'config', value: JsonOutput.toJson(config) ]  ], propagate:false
 
-    node {
-        String text = "<h2>Regression test</h2><a href=\"${testJob.getAbsoluteUrl()}\">${testJob.getProjectName()} ${testJob.getDisplayName()} - ${testJob.getResult()}</a>"
-        rtp(nullAction: '1', parserName: 'HTML', stableText: text, abortedAsStable: true, failedAsStable: true, unstableAsStable: true)
-    }
+    String text = "<h2>Regression test</h2><a href=\"${testJob.getAbsoluteUrl()}\">${testJob.getProjectName()} ${testJob.getDisplayName()} - ${testJob.getResult()}</a>"
+    rtp(nullAction: '1', parserName: 'HTML', stableText: text, abortedAsStable: true, failedAsStable: true, unstableAsStable: true)
 
     if( testJob.getResult() != "SUCCESS" ) {
         error "System test failed"
