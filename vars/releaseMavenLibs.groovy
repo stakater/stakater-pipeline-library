0b37b8648eee9546bcfdfdad12cf54673c613321
kahootali <kahoot.ali@gmail.com>
2018-12-28 19:44:57 +0500

    fix branch being null

--- vars/releaseMavenLibs.groovy
+++ vars/releaseMavenLibs.groovy
@@ -19,9 +19,7 @@ def call(body) {
 
         // Slack variables
         def slackChannel = "${env.SLACK_CHANNEL}"
-        def slackWebHookURL = "${env.SLACK_WEBHOOK_URL}"
-
-        def prNumber = "${env.REPO_BRANCH}"                        
+        def slackWebHookURL = "${env.SLACK_WEBHOOK_URL}"        
 
         def version = ""
 
@@ -29,6 +27,10 @@ def call(body) {
             withCurrentRepo() { def repoUrl, def repoName, def repoOwner, def repoBranch ->                
                 def imageName = repoName.toLowerCase()
                 def fullAppNameWithVersion = ""
+                def prNumber = "${env.REPO_BRANCH}"
+
+                echo "Image NAME: ${imageName}"
+                
                 if (repoOwner.startsWith('stakater-')){
                     repoOwner = 'stakater'
                 }
@@ -37,7 +39,7 @@ def call(body) {
                     stage('Create Version'){
                         // If image Prefix is passed, use it, else pass empty string to create versions
                         def imagePrefix = config.imagePrefix ? config.imagePrefix + '-' : ''                        
-                        version = stakaterCommands.getImageVersionForMavenCiAndCd(repoUrl,imagePrefix, prNumber, "${env.BUILD_NUMBER}")
+                        version = stakaterCommands.getImageVersionForCiAndCd(repoUrl,imagePrefix, prNumber, "${env.BUILD_NUMBER}")                        
                         echo "Version: ${version}"                       
                         fullAppNameWithVersion = imageName + '-'+ version
                     }
@@ -59,14 +61,14 @@ def call(body) {
                     }
                 }
                 catch (e) {
-                    slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)], prNumber)
+                    slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)], prNumber)                    
 
                     def commentMessage = "Yikes! You better fix it before anyone else finds out! [Build ${env.BUILD_NUMBER}](${env.BUILD_URL}) has Failed!"
                     git.addCommentToPullRequest(commentMessage)
 
                     throw e
                 }
-                stage('Notify') {
+                stage('Notify') {                    
                     slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [slack.createArtifactField("${fullAppNameWithVersion}")], prNumber)
 
                     def commentMessage = "Artifact is available for testing. `${fullAppNameWithVersion}`"
