f87cde717918b69cc2e1aa34a353f34015292d4f
faizanahmad055 <faizan.ahmad55@outlook.com>
2019-01-01 18:46:42 +0500

    Uncomment slack notifications
    Signed-off-by: faizanahmad055 <faizan.ahmad55@outlook.com>

--- vars/deployK8sStack.groovy
+++ vars/deployK8sStack.groovy
@@ -9,7 +9,7 @@ def call(body) {
     toolsImage = config.toolsImage ?: 'stakater/pipeline-tools:1.13.2'
     chartName = config.chartName ?: 'global'
     runPreInstall = config.runPreInstall ?: false
-    notifyOnSlack = config.notifyOnSlack ?: true
+    notifyOnSlack = !config.notifyOnSlack ? config.notifyOnSlack : true
 
     toolsWithCurrentKubeNode(toolsImage: toolsImage) {
         container(name: 'tools') {
@@ -72,24 +72,24 @@ def call(body) {
                     }
                     
 
-                    // if (notifyOnSlack){
-                    //     stage('Notify') {
-                    //         def message
-                    //         if (utils.isCD()) {
-                    //             message = "Release ${repoName} ${version}"
-                    //         }
-                    //         else {
-                    //             message = "Dry Run successful"
-                    //         }
-                    //         slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [slack.createField("Message", message, false)])
+                    if (notifyOnSlack){
+                        stage('Notify') {
+                            def message
+                            if (utils.isCD()) {
+                                message = "Release ${repoName} ${version}"
+                            }
+                            else {
+                                message = "Dry Run successful"
+                            }
+                            slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [slack.createField("Message", message, false)])
 
-                    //         git.addCommentToPullRequest(message)
-                    //     }
-                    // }
+                            git.addCommentToPullRequest(message)
+                        }
+                    }
                 } catch(e) {
-                    // if (notifyOnSlack){
-                    //     slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)])
-                    // }
+                    if (notifyOnSlack){
+                        slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)])
+                    }
 
                     def commentMessage = "[Build ${env.BUILD_NUMBER}](${env.BUILD_URL}) has Failed!"
                     git.addCommentToPullRequest(commentMessage)
