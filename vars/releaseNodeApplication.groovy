7da0ad0fd26c6a8024598594c1a0e347e5f0d85c
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2019-05-15 12:38:18 +0500

    remove withCurrentRepo

--- vars/releaseNodeApplication.groovy
+++ vars/releaseNodeApplication.groovy
@@ -63,117 +63,115 @@ def call(body) {
                 String version = ""
 
                 container(name: 'tools') {
-                    withCurrentRepo(gitUsername: gitUser, gitEmail: gitEmailID) { def repoUrl, def repoName, def repoOwner, def repoBranch ->
-                        String kubernetesDir = WORKSPACE + "/deployments/kubernetes"
+                    String kubernetesDir = WORKSPACE + "/deployments/kubernetes"
 
-                        String imageName = repoName.split("dockerfile-").last().toLowerCase()
-                        String fullAppNameWithVersion = ""
+                    String imageName = repoName.split("dockerfile-").last().toLowerCase()
+                    String fullAppNameWithVersion = ""
 
-                        String prNumber = "${env.REPO_BRANCH}"
+                    String prNumber = "${env.REPO_BRANCH}"
 
-                        echo "Image NAME: ${imageName}"
-                        if (repoOwner.startsWith('stakater-')){
-                            repoOwner = 'stakater'
+                    echo "Image NAME: ${imageName}"
+                    if (repoOwner.startsWith('stakater-')){
+                        repoOwner = 'stakater'
+                    }
+                    echo "Repo Owner: ${repoOwner}"
+                    try {
+                        stage('Create Version'){
+                            dockerImage = "${dockerRepositoryURL}/${repoOwner.toLowerCase()}/${imageName}"
+                            // If image Prefix is passed, use it, else pass empty string to create versions
+                            String imagePrefix = config.imagePrefix ? config.imagePrefix + '-' : ''
+                            version = stakaterCommands.getImageVersionForNodeCiAndCd(repoUrl,imagePrefix, prNumber, "${env.BUILD_NUMBER}")
+                            echo "Version: ${version}"
+                            fullAppNameWithVersion = imageName + '-'+ version
                         }
-                        echo "Repo Owner: ${repoOwner}"
-                        try {
-                            stage('Create Version'){
-                                dockerImage = "${dockerRepositoryURL}/${repoOwner.toLowerCase()}/${imageName}"
-                                // If image Prefix is passed, use it, else pass empty string to create versions
-                                String imagePrefix = config.imagePrefix ? config.imagePrefix + '-' : ''
-                                version = stakaterCommands.getImageVersionForNodeCiAndCd(repoUrl,imagePrefix, prNumber, "${env.BUILD_NUMBER}")
-                                echo "Version: ${version}"
-                                fullAppNameWithVersion = imageName + '-'+ version
-                            }
-                            stage('Build Node Application') {
-                                echo "Building Node application"
-                                builder.buildNodeApplication(version)
-                            }
-                            stage('Image build & push') {
-                                sh """
-                                    export DOCKER_IMAGE=${dockerImage}
-                                    export DOCKER_TAG=${version}
-                                """
-                                docker.buildImageWithTagCustom(dockerImage, version)
-                                docker.pushTagCustom(dockerImage, version)
-                            }
+                        stage('Build Node Application') {
+                            echo "Building Node application"
+                            builder.buildNodeApplication(version)
+                        }
+                        stage('Image build & push') {
+                            sh """
+                                export DOCKER_IMAGE=${dockerImage}
+                                export DOCKER_TAG=${version}
+                            """
+                            docker.buildImageWithTagCustom(dockerImage, version)
+                            docker.pushTagCustom(dockerImage, version)
+                        }
 
+                        if (! chartRepositoryURL.equals("")) {
+                            stage('Package chart') {
+                                chartPackageName = chartManager.packageChart(repoName, version, dockerImage, kubernetesDir)
+                            }
+                        }
+
+                        stage('Run Synthetic/E2E Tests') {
+                            echo "Running synthetic tests for Node application:  ${e2eTestJob}"   
+                            if (!e2eTestJob.equals("")){
+                                e2eTestStage(appName: appName, e2eJobName: e2eTestJob, performanceTestJobName: performanceTestsJob, chartName: repoName.toLowerCase(), chartVersion: helmVersion, repoUrl: repoUrl, repoBranch: repoBranch, chartRepositoryURL: chartRepositoryURL, mockAppsJobName: mockAppsJobName, [
+                                    microservice: [
+                                            name   : repoName.toLowerCase(),
+                                            version: helmVersion
+                                    ]
+                                ])
+                            }else{
+                                echo "No Job Name passed."
+                            }
+                        }
+                        // If master
+                        if (utils.isCD()) {
+                            stage("Push Changes") {
+                                print "Pushing changes to Git"
+                                git.commitChanges(WORKSPACE, "Update chart and version")
+                            }
                             if (! chartRepositoryURL.equals("")) {
-                                stage('Package chart') {
-                                    chartPackageName = chartManager.packageChart(repoName, version, dockerImage, kubernetesDir)
+                                stage('Upload Helm Chart') {
+                                    chartManager.uploadChart(chartRepository, chartRepositoryURL, kubernetesDir,
+                                                nexusChartRepoName, repoName, chartPackageName)
                                 }
                             }
 
-                            stage('Run Synthetic/E2E Tests') {
-                                echo "Running synthetic tests for Node application:  ${e2eTestJob}"   
-                                if (!e2eTestJob.equals("")){
-                                    e2eTestStage(appName: appName, e2eJobName: e2eTestJob, performanceTestJobName: performanceTestsJob, chartName: repoName.toLowerCase(), chartVersion: helmVersion, repoUrl: repoUrl, repoBranch: repoBranch, chartRepositoryURL: chartRepositoryURL, mockAppsJobName: mockAppsJobName, [
-                                        microservice: [
-                                                name   : repoName.toLowerCase(),
-                                                version: helmVersion
-                                        ]
-                                    ])
-                                }else{
-                                    echo "No Job Name passed."
+                            stage("Create Git Tag"){
+                                print "Pushing Tag ${version} to Git"
+                                git.createTagAndPush(WORKSPACE, version)
+                            }
+
+                            if (deployManifest) {
+                                stage("Deploy") {
+                                    sh """
+                                        make deploy IMAGE_NAME=${dockerImage} IMAGE_TAG=${version} NAMESPACE=${namespace}
+                                    """
                                 }
                             }
-                            // If master
-                            if (utils.isCD()) {
-                                stage("Push Changes") {
-                                    print "Pushing changes to Git"
-                                    git.commitChanges(WORKSPACE, "Update chart and version")
-                                }
-                                if (! chartRepositoryURL.equals("")) {
-                                    stage('Upload Helm Chart') {
-                                        chartManager.uploadChart(chartRepository, chartRepositoryURL, kubernetesDir,
-                                                    nexusChartRepoName, repoName, chartPackageName)
-                                    }
-                                }
 
-                                stage("Create Git Tag"){
-                                    print "Pushing Tag ${version} to Git"
-                                    git.createTagAndPush(WORKSPACE, version)
-                                }
-
-                                if (deployManifest) {
-                                    stage("Deploy") {
-                                        sh """
-                                            make deploy IMAGE_NAME=${dockerImage} IMAGE_TAG=${version} NAMESPACE=${namespace}
-                                        """
-                                    }
-                                }
-
-                                stage("Push to Dev-Apps Repo"){
-                                    build job: devAppsJobName, parameters: [ [$class: 'StringParameterValue', name: 'chartVersion', value: helmVersion ], [$class: 'StringParameterValue', name: 'chartName', value: repoName.toLowerCase() ], [$class: 'StringParameterValue', name: 'chartUrl', value: chartRepositoryURL ], [$class: 'StringParameterValue', name: 'chartAlias', value: repoName.toLowerCase() ]]
-                                }
+                            stage("Push to Dev-Apps Repo"){
+                                build job: devAppsJobName, parameters: [ [$class: 'StringParameterValue', name: 'chartVersion', value: helmVersion ], [$class: 'StringParameterValue', name: 'chartName', value: repoName.toLowerCase() ], [$class: 'StringParameterValue', name: 'chartUrl', value: chartRepositoryURL ], [$class: 'StringParameterValue', name: 'chartAlias', value: repoName.toLowerCase() ]]
                             }
                         }
-                        catch (e) {
-                            if (notifySlack) {
-                                slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)], prNumber)
-                            }
-
-                            String commentMessage = "Yikes! You better fix it before anyone else finds out! [Build ${env.BUILD_NUMBER}](${env.BUILD_URL}) has Failed!"
-
-                            if(cloneUsingToken) {
-                                git.addCommentToPullRequest(commentMessage, tokenSecret)
-                            } else {
-                                git.addCommentToPullRequest(commentMessage)
-                            }
-                            throw e
+                    }
+                    catch (e) {
+                        if (notifySlack) {
+                            slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)], prNumber)
                         }
-                        stage('Notify') {
-                            if (notifySlack) {
-                                slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [slack.createDockerImageField("${dockerImage}:${version}")], prNumber)
-                            }
 
-                            String commentMessage = "Image is available for testing. `docker pull ${dockerImage}:${version}`"
+                        String commentMessage = "Yikes! You better fix it before anyone else finds out! [Build ${env.BUILD_NUMBER}](${env.BUILD_URL}) has Failed!"
 
-                            if(cloneUsingToken){
-                                git.addCommentToPullRequest(commentMessage, tokenSecret)
-                            } else {
-                                git.addCommentToPullRequest(commentMessage)
-                            }
+                        if(cloneUsingToken) {
+                            git.addCommentToPullRequest(commentMessage, tokenSecret)
+                        } else {
+                            git.addCommentToPullRequest(commentMessage)
+                        }
+                        throw e
+                    }
+                    stage('Notify') {
+                        if (notifySlack) {
+                            slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [slack.createDockerImageField("${dockerImage}:${version}")], prNumber)
+                        }
+
+                        String commentMessage = "Image is available for testing. `docker pull ${dockerImage}:${version}`"
+
+                        if(cloneUsingToken){
+                            git.addCommentToPullRequest(commentMessage, tokenSecret)
+                        } else {
+                            git.addCommentToPullRequest(commentMessage)
                         }
                     }
                 }
