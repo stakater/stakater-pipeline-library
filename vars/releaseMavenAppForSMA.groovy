9014b5a2f51944f4ee32c4f21705599f77503273
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-08-23 18:59:02 +0500

    update builder-maven's pipeline-tools version

--- vars/releaseMavenAppForSMA.groovy
+++ vars/releaseMavenAppForSMA.groovy
@@ -8,7 +8,7 @@ def call(body) {
     body()
 
     def stakaterPod = new io.stakater.pods.Pod()
-    stakaterPod.setToolsImage(config, "stakater/builder-maven:3.5.4-jdk1.8-v2.0.1-v0.0.7")
+    stakaterPod.setToolsImage(config, "stakater/builder-maven:3.5.4-jdk1.8-v2.0.9-v0.0.8")
     stakaterPod.setDockerConfig(config)
     stakaterPod.enableMavenSettings(config)
 
