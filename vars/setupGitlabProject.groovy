46f6126fcd839d2004c79b2ba89403c1af58cabe
kahootali <kahoot.ali@gmail.com>
2019-09-11 12:47:00 +0500

    remove extra vars

--- vars/setupGitlabProject.groovy
+++ vars/setupGitlabProject.groovy
@@ -6,17 +6,13 @@ def call(body) {
     body.delegate = config
     body()
 
-    toolsNode(toolsImage: "stakater/pipeline-tools:1.13.2") {
+    stakaterToolsNode(toolsImage: "stakater/pipeline-tools:1.13.2") {
         container(name: "tools") {
             withCurrentRepo(gitUsername: config.gitUserName, gitEmail: config.gitEmail) { def repoUrl, def repoName, def repoOwner, def repoBranch ->
                 def utils = new io.fabric8.Utils()
                 def slack = new io.stakater.notifications.Slack()
                 def git = new io.stakater.vc.Git()
-                def terraform = new io.stakater.automation.Terraform()
-
-                // Slack variables
-                def slackChannel = "${env.SLACK_CHANNEL}"
-                def slackWebHookURL = "${env.SLACK_WEBHOOK_URL}"
+                def terraform = new io.stakater.automation.Terraform()                
 
                 def exportKey
                 def exportValue
@@ -137,17 +133,14 @@ def call(body) {
                     }
                     }
 
-                    stage('Notify') {
-                    slack.sendDefaultSuccessNotification(slackWebHookURL, slackChannel, [])
+                    stage('Notify') {                    
 
                     def commentMessage = "Terraform ${utils.isCD() ? "Apply" : "Validate"} successful"
                     git.addCommentToPullRequest(commentMessage)
                     }
 
                 }
-                catch(e) {
-                    slack.sendDefaultFailureNotification(slackWebHookURL, slackChannel, [slack.createErrorField(e)])
-
+                catch(e) {                
                     def commentMessage = "Yikes! You better fix it before anyone else finds out! [Build ${env.BUILD_NUMBER}](${env.BUILD_URL}) has Failed!"
                     git.addCommentToPullRequest(commentMessage)
                     sh """
