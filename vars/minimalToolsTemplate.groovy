b58583a092553b7daab270de9964baf15452b454
irti <aliartiza75@yahoo.com>
2019-11-07 12:59:09 +0500

    [update-toolsNode] update manifest vars/minimalToolsTemplate.groovy

--- vars/minimalToolsTemplate.groovy
+++ vars/minimalToolsTemplate.groovy
@@ -52,7 +52,7 @@ def call(Map parameters = [:], body) {
                     persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
                     secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                     // secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                    // secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                    secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                     // secretVolume(secretName: 'ingress-monitor-controller-test-config', mountPath: '/etc/ingress-monitor-controller'),
                     // secretVolume(secretName: 'stk-config', mountPath: '/home/jenkins/.stk'),
                     // secretVolume(secretName: 'helm-pgp-key', mountPath: '/usr/local/bin/pgp-configuration/'),
