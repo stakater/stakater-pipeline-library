1755091e039da40ba32e851ba842e6febcce76b4
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-11-12 17:54:09 +0500

    debug params

--- vars/minimalToolsTemplate.groovy
+++ vars/minimalToolsTemplate.groovy
@@ -11,6 +11,7 @@ def call(Map parameters = [:], body) {
     def toolsImage = parameters.get('toolsImage', 'stakater/pipeline-tools:v2.0.5')
     def notificationSecret = parameters.get('notificationSecret', 'slack-notification-hook')
     echo "===================="
+    echo "${parameters}"
     echo "${notificationSecret}"
     echo "===================="
     def inheritFrom = parameters.get('inheritFrom', 'base')
