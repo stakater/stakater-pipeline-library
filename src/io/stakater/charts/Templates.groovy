a98e3aee8b8f60a83eb4f3ba8166ba7b7ac1c2c2
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-02-10 18:02:39 +0500

    Refactored pipeline

--- src/io/stakater/charts/Templates.groovy
+++ src/io/stakater/charts/Templates.groovy
@@ -96,7 +96,6 @@ def generateManifestsUsingValues(String chartRepoUrl, String chartName, String c
             chartName=${chartName}
             chartDirectoryName=\${chartName##*/}
             chartDirectory=${deploymentsDir}/\${chartDirectoryName}
-            sleep 3000
             helm template -f \${valueFile} --namespace ${namespace} --output-dir "\${manifestsDir}" "\${chartDirectory}"
             helm template \${chartDirectory} -f \${valueFile} --namespace ${namespace} > \${manifestsDir}/\${chartDirectoryName}/${appName}.yaml
             rm -rf \${deploymentsDir}/\${chartDirectoryName}
