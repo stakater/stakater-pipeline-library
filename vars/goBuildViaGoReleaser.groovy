e580903dda891001109b350c4450e684b106ee21
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-03-11 13:55:19 +0500

    Update default pipeline-tools version (#55)

--- vars/goBuildViaGoReleaser.groovy
+++ vars/goBuildViaGoReleaser.groovy
@@ -6,7 +6,7 @@ def call(body) {
     body.delegate = config
     body()
 
-    toolsImage = config.toolsImage ?: 'stakater/pipeline-tools:v2.0.16'
+    toolsImage = config.toolsImage ?: 'stakater/pipeline-tools:v2.0.18'
 
     toolsNode(toolsImage: toolsImage) {
         container(name: 'tools') {
